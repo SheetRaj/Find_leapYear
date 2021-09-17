5603f20d11c342c16a8aaf68209a264edf026329
sheet raj <samuleclassi06@gmail.com>
2021-09-17 07:05:02 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int year;
+    printf("Enter any year: ");
+    scanf("%d", &year);
+
+    if(year%4==0){
+        if(year%100==0){
+            //year is divisible by 400, hence the year is a leap year
+            if(year%400==0){
+                printf("%d is a leap year.", year);
+            }
+            else{
+                printf("%d is not a leap year.", year);
+            }
+
+        }
+        else{
+            printf("%d is a leap year.", year);
+        }
+
+    }
+    else{
+        printf("%d is not a leap year.", year);
+    }
+    return 0;
+}
